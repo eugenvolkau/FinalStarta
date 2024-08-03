d10d5d39234dfbff2f288395dbf1f781e38c519f
Eugen <eugen.volkau@gmail.com>
2024-08-03 13:28:50 +0200

    first commit

---
+++ src/main/java/telran/org/scotlandyard/ScotlandyardApplication.java
@@ -0,0 +1,13 @@
+package telran.org.scotlandyard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScotlandyardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScotlandyardApplication.class, args);
+    }
+
+}
